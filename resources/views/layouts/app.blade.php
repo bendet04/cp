20cf10c885ae98d61740f99dc0053aad671f052b
Adi <adi.priyatna@coway.id>
2022-01-18 23:07:12 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,12 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('products') }}">{{ __('Produk') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('product-category') }}">{{ __('Kategory Produk') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('news-category') }}">{{ __('Kategory Berita') }}</a>
+                        </li>
                         @endguest
                     </ul>
 
