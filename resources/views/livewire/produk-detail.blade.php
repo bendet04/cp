6e131599dc0965ae5b3d7f8ff60799fca737b6c0
Adi <adi.priyatna@coway.id>
2022-01-23 15:57:26 +0700

    test

--- resources/views/livewire/produk-detail.blade.php
+++ resources/views/livewire/produk-detail.blade.php
@@ -1,70 +1,61 @@
 <div class="productpart1">
     <div class="container2">
-        <table cellpadding="0" cellspacing="0" border="0" width="100%" class="tableproductpart1">
+        <table cellpadding="0" cellspacing="0" border="0" width="100%" class="tableproductpart2">
             <tr>
-                
-                <td class='tdproductpart1 tdproductpart1_2'>
-                    <div class="padbot80">
-                        <table cellpadding="0" cellspacing="0" border="0" width="100%" class="tableproductpart2">
+                <td class="tdproductpart2 tdproductpart2_1">
+                    <div class="main_image_div" id="main_image_div" align="center">
+                        <img src="{{asset('storage/'.$product->image)}}" class="imgproduct imgdp borderradius5"
+                            id="zoom_01" data-zoom-image="/images/product/phpzqz4k7_resized.png" /> </div>
+                    <div style="height:10px"></div>
+                </td>
+                <td class="tdproductpart2 tdproductpart2_2 ">
+                    <div class="padbot40"></div>
+                    <div class="pagetitle3b padbot20">{{$product->name}}</div>
+
+                    <div class="padbot15">
+                        <div class="line1"></div>
+                    </div>
+
+                    <div class="pagedescription1 padbot30">
+                        <p style="width:50px;">{{$product->description}}</p>
+                    </div>
+
+                    <div class=" padbot30">
+                        <table cellpadding="0" cellspacing="0" border="0" class="table_dp_contact">
                             <tr>
-                                <td class="tdproductpart2 tdproductpart2_1">
-                                    <div class="main_image_div" id="main_image_div" align="center">
-                                        <img src="{{asset('storage/'.$product->image)}}"
-                                            class="imgproduct imgdp borderradius5" id="zoom_01"
-                                            data-zoom-image="/images/product/phpzqz4k7_resized.png" /> </div>
-                                    <div style="height:10px"></div>
+                                <td class="td_dp_contact td_dp_contact1">
+                                    <img src="{{asset('storage/images/phone_icon2.png')}}" />
                                 </td>
-                                <td class="tdproductpart2 tdproductpart2_2">
-                                    <div class="padbot40"></div>
-                                    <div class="pagetitle3b padbot20">{{$product->name}}</div>
-
-                                    <div class="padbot15">
-                                        <div class="line1"></div>
-                                    </div>
-
-                                    <div class="pagedescription1 padbot30">
-                                        {{$product->description}}
-                                    </div>
-
-                                    <div class=" padbot30">
-                                        <table cellpadding="0" cellspacing="0" border="0" class="table_dp_contact">
-                                            <tr>
-                                                <td class="td_dp_contact td_dp_contact1">
-                                                    <img src="{{asset('storage/images/phone_icon2.png')}}" />
-                                                </td>
-                                                <td class="td_dp_contact td_dp_contact2">
-                                                (0274) 2801021 </td>
-                                            </tr>
-                                            <tr>
-                                                <td class="td_dp_contact td_dp_contact1">
-                                                    <img src="{{asset('storage/images/wa_icon2.png')}}" />
-                                                </td>
-                                                <td class="td_dp_contact td_dp_contact2">
-                                                +62877-7300-0454 </td>
-                                            </tr>
-                                            <tr>
-                                                <td class="td_dp_contact td_dp_contact1">
-                                                    <img src="{{asset('storage/images/mail_icon.png')}}" />
-                                                </td>
-                                                <td class="td_dp_contact td_dp_contact2">
-                                                    <a
-                                                        href="mailto:percetakanharfeey@gmail.com">percetakanharfeey@gmail.com</a>
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </div>
-
-                                    <a href="https://wa.me/6285899991818">
-                                        <div class="btn3 animationall borderradius5">
-                                            <div class="vm"><img src="{{asset('storage/images/wa_float.png')}}" /> Chat kami sekarang</div>
-                                        </div>
-                                    </a>
+                                <td class="td_dp_contact td_dp_contact2">
+                                    (0274) 2801021 </td>
+                            </tr>
+                            <tr>
+                                <td class="td_dp_contact td_dp_contact1">
+                                    <img src="{{asset('storage/images/wa_icon2.png')}}" />
+                                </td>
+                                <td class="td_dp_contact td_dp_contact2">
+                                    +62877-7300-0454 </td>
+                            </tr>
+                            <tr>
+                                <td class="td_dp_contact td_dp_contact1">
+                                    <img src="{{asset('storage/images/mail_icon.png')}}" />
+                                </td>
+                                <td class="td_dp_contact td_dp_contact2">
+                                    <a href="mailto:percetakanharfeey@gmail.com">percetakanharfeey@gmail.com</a>
                                 </td>
                             </tr>
                         </table>
                     </div>
+
+                    <a href="https://wa.me/6285899991818">
+                        <div class="btn3 animationall borderradius5">
+                            <div class="vm"><img src="{{asset('storage/images/wa_float.png')}}" /> Chat kami sekarang
+                            </div>
+                        </div>
+                    </a>
                 </td>
             </tr>
         </table>
+
     </div>
 </div>
